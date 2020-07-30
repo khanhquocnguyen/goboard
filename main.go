51e54fb97bffd549d2fa036a811a9039b7a09691
Khanh Quoc Nguyen <13175175+khanhquocnguyen@users.noreply.github.com>
2020-07-30 12:12:27 +0700

    Complete backend api

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -16,12 +18,101 @@ type Task struct {
 	Status      string
 }
 
+type TaskList struct {
+	Items []Task
+}
+
 var db *sql.DB
 var err error
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers", "content-type")
+
+	stmt, err := db.Prepare("INSERT INTO tasks (description, status) VALUES(?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rqbody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyval := make(map[string]string)
+	json.Unmarshal(rqbody, &keyval)
+	description := keyval["description"]
+	status := keyval["status"]
+
+	_, err = stmt.Exec(description, status)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "New task was created")
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	res, err := db.Query("SELECT id, description, status FROM tasks WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Close()
+
+	var task Task
+	for res.Next() {
+		err := res.Scan(&task.Id, &task.Description, &task.Status)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE tasks SET description = ?, status = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyval := make(map[string]string)
+	json.Unmarshal(body, &keyval)
+	newDescription := keyval["description"]
+	newStatus := keyval["status"]
+	_, err = stmt.Exec(newDescription, newStatus, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Task %s was updated", params["id"])
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM tasks WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Task %s was deleted", params["id"])
+}
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var tasks []Task
+	var thelist TaskList
 
 	result, err := db.Query("SELECT id, description, status from tasks")
 	if err != nil {
@@ -37,8 +128,9 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 		}
 		tasks = append(tasks, task)
 	}
+	thelist.Items = tasks
 
-	json.NewEncoder(w).Encode(tasks)
+	json.NewEncoder(w).Encode(thelist)
 }
 
 func main() {
@@ -50,10 +142,10 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/tasks", getTasks).Methods("GET")
-	//router.HandleFunc("/tasks", createTask).Methods("POST")
-	//router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
-	//router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
-	//router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks", createTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 
 	log.Printf("Listening on 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
